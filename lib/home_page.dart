07547cef27c78d4c6b199e503d9c398b0ff340d0
usmanmubashir <usmanmubasheir@gmail.com>
2023-07-17 02:53:34 +0500

    day 6

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,28 +1,163 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_day1/utilties/routs.dart';
 
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class _HomePageState extends State<HomePage> {
+  bool containerAnimation = false;
+  bool containerAnimation1 = false;
+  bool containerAnimation2 = false;
 
+  // const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-
     var ab = "A";
 
     return Scaffold(
-      
       appBar: AppBar(
-        title: Text("Allah"),
+        // title: Text("Allah"),
+        title: Text("News App"),
       ),
 
       drawer: Drawer(
-        child: Center(
-          child: Text("Container")),),
-
-      body: Center(
-        child: Container(
-          child: Text("Allah is the $ab Greater"),
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("ABC"),
+              accountEmail: Text("johndoe@example.com"),
+              currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage(
+                "assets/images/profile.jpg",
+              )),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.indigo,
+              ),
+              title: Text("Home"),
+              onTap: () {
+                Navigator.pushNamed(context, MyRouts.homeRoute);
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.wallet,
+                color: Colors.indigo,
+              ),
+              title: Text("Wallet"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.indigo,
+              ),
+              title: Text("Settings"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.logout,
+                color: Colors.indigo,
+              ),
+              title: Text("Logout"),
+              onTap: () {},
+            )
+          ],
         ),
+      ),
+
+      // body: Center(
+      //   child: Container(
+      //     child: Text("Allah is the $ab Greater"),
+      //   ),
+      // ),
+
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 10),
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  containerAnimation = true;
+                });
+              },
+              child: Container(
+                width: double.infinity,
+                height: 150,
+                // color: Colors.indigo[50],
+                decoration: BoxDecoration(
+                    color: containerAnimation
+                        ? Colors.indigo[100]
+                        : Colors.indigo[50],
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+            child: Row(
+              children: [
+                Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.fromLTRB(13, 0, 6.5, 0),
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        containerAnimation1 = true;
+                      });
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      height: 150,
+                      // color: Colors.amber[100],
+                      decoration: BoxDecoration(
+                          color: containerAnimation1
+                              ? Colors.amber[200]
+                              : Colors.amber[100],
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  ),
+                )),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(6.5, 0, 13, 0),
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          containerAnimation2 = true;
+                        });
+                      },
+                      child: AnimatedContainer(
+                        child: Center(child: 
+                        Text("INTERNATIONAL",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green
+                        ),
+                        )
+                        ),
+                        duration: Duration(seconds: 1),
+                        height: 150,
+                        decoration: BoxDecoration(
+                          color: containerAnimation2
+                              ? Colors.green[100]
+                              : Colors.green[50],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,89 +2,85 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_day1/utilties/routs.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
-
-class Loginpage extends StatelessWidget {
+class Loginpage extends StatefulWidget {
   const Loginpage({super.key});
+
+  @override
+  State<Loginpage> createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  String name = "";
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-    child: Column(
-      children: [
-        Image.asset(
-          "assets/images/login.png",
-        fit: BoxFit.contain, 
-        width: 200, 
-        height: 200,
-        ),
-
-
-        Text(
-          "Login",
-        style: TextStyle(
-          fontSize: 35,
-          color: Colors.teal,
-          fontWeight: FontWeight.w400,
-          fontFamily: GoogleFonts.ubuntu().fontFamily,
-        ),
-        ),
-        
-
-        SizedBox(
-          height: 35,
-        ),
-
-
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 0),
-          child: Column(
-            children: [
-
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: "User Name",
-              hintText: "Enter User Name"
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
             ),
-        ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Password",
-                hintText: "Enter Password"
-              ),
+            Image.asset(
+              "assets/images/news.png",
+              fit: BoxFit.contain,
+              width: 200,
+              height: 200,
             ),
-              ),
-
-
-SizedBox(
-  height: 25,
-),
-              ElevatedButton(onPressed: (){
-                Navigator.pushNamed(context, MyRouts.homeRoute);
-              }, 
-
-            style: ElevatedButton.styleFrom(
-              primary: Colors.teal,
-              textStyle: TextStyle(
+            Text(
+              "WELLCOME $name ",
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.indigo,
+                fontWeight: FontWeight.w400,
                 fontFamily: GoogleFonts.ubuntu().fontFamily,
-                
-                
-              )
+              ),
             ),
-
-              child: Text("LOGIN"))
-            ],
-        
-        
-          ),
-        )
-
-      ],
-    ),
+            SizedBox(
+              height: 35,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "User Name", hintText: "Enter User Name"),
+                    onChanged: (value) {
+                      setState(() {
+                        name = value;
+                      });
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          labelText: "Password", hintText: "Enter Password"),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  ElevatedButton(
+                      onPressed: () async {
+                        await Future.delayed(Duration(seconds: 1));
+                        Navigator.pushNamed(context, MyRouts.homeRoute);
+                      },
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.indigo,
+                          textStyle: TextStyle(
+                            fontFamily: GoogleFonts.ubuntu().fontFamily,
+                          )),
+                      child: Text("LOGIN"))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
